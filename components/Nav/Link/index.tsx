a8565e27e18e80e2f954b7eee5bb2219e9c8b5b0
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2022-05-22 01:25:17 -0500

    feat(Nav): add Nav links
    Add Nav Links, not finished yet...

---
+++ components/Nav/Link/index.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import styles from "./link.module.scss";
+
+type Props = {
+  text: string;
+  href: string;
+  _className?: string;
+};
+
+export default function ({ text, href, _className }: Props) {
+  return (
+    <Link passHref href={href}>
+      <a className={`${_className} ${styles.link_style}`}>{text}</a>
+    </Link>
+  );
+}
--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -1,9 +1,22 @@
 import styles from "./nav.module.scss";
 
+import LogoIcon from "../Icons/Logo/index";
+
+import Link from "./Link/index";
+
 export default function Layout() {
   return (
     <nav className={styles.nav}>
-      Hola Nav
+      <div>
+        <LogoIcon _className={styles.logo} />
+      </div>
+      <div className={styles.nav_links}>
+        <Link href="" text="INICIO" />
+        <Link href="" text="NOSOTROS" />
+        <Link href="" text="SERVICIOS" />
+        <Link href="" text="GALERÍA" />
+      </div>
+      <div>social</div>
     </nav>
   );
 }
