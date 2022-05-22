56293cc536634b53e9e78effb23f2da445a75760
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2022-05-22 16:31:56 -0500

    feat(History/Index): complete History/Index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/Home/Header/index";
+import History from "../components/Home/History/index";
 
 export default function () {
   return (
     <div>
       <Header />
+      <History />
     </div>
   );
 }
