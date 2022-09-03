a576ee4e60516b4e3893767386f0421ddf7e02bf
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2022-09-02 19:29:19 -0500

    fix images

--- pages/galeria.tsx
+++ pages/galeria.tsx
@@ -1,5 +1,6 @@
 import Header from "../components/Gallery/Header/index";
 import Images from "../components/Gallery/Images/index";
+import Footer from "../components/Footer/index";
 
 export default function Component() {
   return (
@@ -10,6 +11,7 @@ export default function Component() {
     >
       <Header />
       <Images />
+      <Footer />
     </div>
   );
 }
